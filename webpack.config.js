5dfd3487f3e42fbdc0e1cd6a8e3e76a229381d41
gitachyut <linkinakd72@gmail.com>
2016-10-31 01:05:19 +0530

    webpack edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require("webpack");
 var BowerWebpackPlugin = require('bower-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var path = require('path');
 module.exports = {
    entry: './js/index.js',
    output: {
@@ -25,8 +27,9 @@ module.exports = {
        $:      "jquery",
        jQuery: "jquery"
      }),
-     new ExtractTextPlugin('css/style.css', {
-            allChunks: true
-     })
+     new ExtractTextPlugin('./css/style.css'),
+     new webpack.optimize.UglifyJsPlugin({
+           compress: { warnings: false }
+    })
    ]
  };
